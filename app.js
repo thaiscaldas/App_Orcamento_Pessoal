c7f410ac1098bc9a0741a00a4feb947888dcb46e
Thais Caldas <caldasthais097@gmail.com>
2025-01-07 15:58:01 -0300

    listando despesas

--- app.js
+++ app.js
@@ -21,22 +21,39 @@ class Despesa {
 class Bd {
     constructor() {
         let id = localStorage.getItem('id')
-
         if(id === null) {
             localStorage.setItem('id', 0)
         }
     }
 
     getProximoId() {
-        let proximoId = localStorage.getItem('id')
+        let proximoId = parseInt(localStorage.getItem('id'))
         return parseInt(proximoId) + 1
     }
     gravar(d) {
         let id = this.getProximoId()
-
         localStorage.setItem(id, JSON.stringify(d))
-
         localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+        let despesas = Array()
+        let id = parseInt(localStorage.getItem('id'))
+
+        //recuperar todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+            console.log(`Despesa recuperada com ID ${i}:`, despesa)
+            //Se haver índices que foram pulados/removidos o sistema pode pular esses índices
+            if(despesa === null) {
+                continue
+            }
+            
+            despesas.push(despesa)
+        }
+        return despesas
     }
 }
 
@@ -59,9 +76,11 @@ function cadastrarDespesa() {
         descricao.value,
         valor.value
     )
-
+    
     if(despesa.validarDados()){
+        
         bd.gravar(despesa)
+        
 
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!'
         document.getElementById('modal_header_div').className = 'modal-header text-success'
@@ -78,4 +97,41 @@ function cadastrarDespesa() {
         $('#modalRegistrarDespesa').modal('show')//mensagem de erro
     }
     
+}
+
+function carregaListaDespesas() {
+    
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+    
+    //selecionando o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //recuperar o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function(d) {
+
+        //criar a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar a coluna (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        //ajustar o tipo
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    }) 
+
 }
